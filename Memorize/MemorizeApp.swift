6b7720e908112ae96fdd7dd9c7068e592a4df015
Badal Aryal <badal.aryal@gmail.com>
2024-05-27 16:33:54 +0545

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Badal  Aryal on 27/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
