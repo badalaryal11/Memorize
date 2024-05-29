57f7e86046a6375473d3209399c8ba2b3f678cd2
Badal Aryal <badal.aryal@gmail.com>
2024-05-29 12:56:35 +0545

    Make Array for Cards with ForEach

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,13 +9,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emojis: Array<String> = ["👻", "🎃", "🕷", "👹", "👹"]
     
     var body: some View {
         HStack{
-           CardView(isFaceUp: true)
-            CardView()
-            CardView(isFaceUp: true)
-            CardView(isFaceUp: true)
+            ForEach(emojis.indices, id: \.self) { index in
+                CardView(content: emojis[index])
+            }
+            
+           
         }
         
         .foregroundColor(.green)
@@ -24,6 +26,7 @@ struct ContentView: View {
 }
 
 struct CardView: View{
+    let content: String
     @State var isFaceUp = true
     
     var body: some View{
@@ -34,7 +37,7 @@ struct CardView: View{
                 
                    base.fill(.white)
                    base.strokeBorder(lineWidth: 2)
-                   Text("👻").font(.largeTitle)
+                   Text(content).font(.largeTitle)
             }else {
                 base.fill()
                    
