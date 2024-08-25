1b881dab1af1e558021067b0ed6fae20e66a7e19
Badal Aryal <badal.aryal@gmail.com>
2024-08-25 15:15:45 +0545

    Add animation offset transition to deal with cards at the beginning of the game.

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -46,13 +46,37 @@ struct EmojiMemoryGameView: View {
     private var cards: some View {
         
         AspectVGrid(viewModel.cards, aspectRatio: aspectRatio) { card in
-            CardView(card)
-                .padding(spacing)
-                .overlay(FlyingNumber(number: scoreChange(causedBy: card)))
-                .onTapGesture {
-                    choose(card)
-                }
+            if isDealt(card) {
+                CardView(card)
+                    .padding(spacing)
+                    .overlay(FlyingNumber(number: scoreChange(causedBy: card)))
+                    .onTapGesture {
+                        choose(card)
+                    }
+                    .transition(.offset(
+                        x:CGFloat.random(in: -1000...1000),
+                        y:CGFloat.random(in: -1000...1000)
+                    ))
+            }
         }
+        .onAppear {
+            // deal the cards
+            withAnimation(.easeInOut(duration: 2)) {
+                for card in viewModel.cards {
+                    dealt.insert(card.id)
+                }
+            }
+            
+        }
+    }
+    @State private var dealt = Set<Card.ID>()
+    
+    private func isDealt(_ card: Card) -> Bool {
+        dealt.contains(card.id)
+    }
+    
+    private var undealtCards: [Card] {
+        viewModel.cards.filter{ !isDealt($0)}
     }
     
     private func choose(_ card: Card) {
